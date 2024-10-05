1af2ebb85c18dc77d5674ec90e7cf5f931e87e02
Yann-Donald <yanndonald237@gmail.com>
2024-10-05 16:17:15 +0100

    3:15:28

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,17 +1,19 @@
+import SearchField from "@/components/SearchField";
 import UserButton from "@/components/UserButton";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="sticky-top top-0 z-10 bg-card shadow-md">
+    <header className="sticky-top top-0 z-10 bg-card shadow-sm">
       <div className="max-w-7xl mx-auto flex items-center justify-center flex-wrap gap-5 px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary ">
           <span className="bg-gradient-to-b from-purple-700 to-purple-500 text-transparent bg-clip-text">
             besties
           </span>
         </Link>
-        <UserButton />
+        <SearchField />
+        <UserButton className="sm:ms-auto" />
       </div>
     </header>
   );
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { useSession } from "@/app/(main)/SessionProvider";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import UseAvatar from "./UseAvatar";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+import Link from "next/link";
+import { LogOutIcon, User2Icon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/action";
+import { cn } from "@/lib/utils";
 
 interface UserButtonProps {
   className?: string;
@@ -13,10 +24,27 @@ const UserButton = ({ className }: UserButtonProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="">
+        <button className={cn("outline-none ring-0", className)}>
           <UseAvatar avatarUrl={user.avatarUrl} size={40} />
         </button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" /> Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
